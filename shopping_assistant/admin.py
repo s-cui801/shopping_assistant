7d52856172c9a03bffc596cbe051a87914825164
s-cui801 <scui059@uOttawa.ca>
2024-11-12 22:19:43 -0500

    Set up the admin page for the Django project

--- shopping_assistant/admin.py
+++ shopping_assistant/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CartItems, Customers, Orders, Products, OrderItems
 
 # Register your models here.
+
+admin.site.register(Products)
+admin.site.register(Customers)
+admin.site.register(CartItems)
+admin.site.register(Orders)
+admin.site.register(OrderItems)
